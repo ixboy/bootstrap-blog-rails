0f0b0bdf782f552da931715e819ca9035dd180aa
ixboy <ismaelixboy@gmail.com>
2022-03-22 02:33:29 +0300

    create articles controller and model and listed them

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_152305) do
+ActiveRecord::Schema.define(version: 2022_03_21_224522) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
