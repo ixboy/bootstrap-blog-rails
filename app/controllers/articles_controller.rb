5c6316486680be2552ac797932fb5fabc7f4dbef
ixboy <ismaelixboy@gmail.com>
2022-03-24 02:35:28 +0300

    fix destroy action not working, refactor code to be dry

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, except: %i[index new create]
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def new
     @article = Article.new
@@ -20,9 +19,30 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
