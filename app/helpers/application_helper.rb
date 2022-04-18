9cd4692d9b9b5d7b778e1687a3fe3fdc59598547
ixboy <ismaelixboy@gmail.com>
2022-04-18 13:59:22 +0300

    add flash msg helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def render_if(condition, template, record)
     render template, record if condition
   end
+
+  def display_msg
+    concat content_tag(:div, alert, class: 'alert alert-danger') if alert.present?
+    content_tag(:div, notice, class: 'alert alert-success') if notice.present?
+  end
 end
