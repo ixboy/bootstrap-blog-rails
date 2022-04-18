656ad3c4697d33a3fef5a47f840a12431ee3bac1
ixboy <ismaelixboy@gmail.com>
2022-04-18 14:09:02 +0300

    refactor articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to @article
+      redirect_to @article, notice: 'Article was successfully created.'
     else
       render :new
     end
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
-      redirect_to @article
+      redirect_to @article, notice: 'Article was successfully updated.'
     else
       render :edit
     end
@@ -36,14 +36,13 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-
-    redirect_to root_path
+    redirect_to root_path, notice: 'Article was successfully destroyed.'
   end
 
   private
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :category_id)
   end
 
   def set_article
