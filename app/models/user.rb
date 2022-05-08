ad3f75b925c9c4e2c6321926c82b70b7617d6d0e
ixboy <ismaelixboy@gmail.com>
2022-05-09 01:51:08 +0300

    add devise user Model and migrations

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  #  :lockable, :timeoutable,  and :omniauthable
+  devise :confirmable,
+         :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,
+         :validatable
+end
