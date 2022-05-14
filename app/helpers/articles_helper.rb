44598f64c1af5882bce806b95ca5578f5ac711ad
ixboy <ismaelixboy@gmail.com>
2022-05-14 08:11:45 +0300

    add helper to edit and remove articles link and remove condition to views

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -10,4 +10,20 @@ module ArticlesHelper
               class: 'btn btn-primary'
     end
   end
+
+  def edit_article_link(article)
+    if policy(article).update?
+      link_to 'Edit', edit_article_path(article),
+              class: 'btn btn-secondary'
+    end
+  end
+
+  def delete_article_link(article)
+    if policy(article).destroy?
+      link_to 'Destroy', article_path(article), data: {
+        method: :delete,
+        confirm: 'Are you sure?'
+      }, class: 'btn btn-danger'
+    end
+  end
 end
