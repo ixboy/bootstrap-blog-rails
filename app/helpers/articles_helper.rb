913b46ef4278768897a3d45aad3e5c9eaff9f4b0
ixboy <ismaelixboy@gmail.com>
2022-05-14 07:55:02 +0300

    refactor link to new article, add helper and remove condition from the view

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,4 +2,12 @@ module ArticlesHelper
   def date_helper(datetime)
     datetime.strftime('%B %e, %Y')
   end
+
+  def new_article_link
+    if current_user
+      link_to 'create new article',
+              new_article_path,
+              class: 'btn btn-primary'
+    end
+  end
 end
