1e228f92f0c90433d829abba4d9d6ccf26177c2d
ixboy <ismaelixboy@gmail.com>
2022-06-07 23:53:44 +0300

    add comments controller and setup create action

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  before_action :set_article
+
+  def create
+    @article.comments.create(comment_params.to_h.merge!({ user_id: current_user.id }))
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
+end
