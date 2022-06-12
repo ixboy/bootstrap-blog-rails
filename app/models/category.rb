6ee164e9767ad65f24951b1ed9ccf57c13f393ff
ixboy <ismaelixboy@gmail.com>
2022-06-12 05:03:07 +0300

    change name validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ApplicationRecord
 
   validates :name,
             presence: true,
-            length: { minimum: 3 },
+            length: { minimum: 1 },
             uniqueness: { case_sensitive: false }
 
   # Scopes
