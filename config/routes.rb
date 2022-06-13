88297f8534cccc6902bcadeaddd4ff486a664210
ixboy <ismaelixboy@gmail.com>
2022-06-13 23:54:08 +0300

    add routes for destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
   resources :articles do
-    resources :comments, only: %i[create]
+    resources :comments, only: %i[create destroy]
   end
   resources :categories, except: :show
 end
