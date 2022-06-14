536248bfe91e09050601c7a3f0fd0408c8888a32
ixboy <ismaelixboy@gmail.com>
2022-06-14 03:09:15 +0300

    create policies for comments delete and update actions

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,4 +1,8 @@
-class CategoryPolicy < ApplicationPolicy
+class CommentPolicy < ApplicationPolicy
+  def update?
+    user&.id == record.user.id
+  end
+
   def destroy?
     user&.id == record.user.id
   end
