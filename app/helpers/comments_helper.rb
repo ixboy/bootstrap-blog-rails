c45aae711a558729cd0ed59a1b03ceb12ed9cf67
ixboy <ismaelixboy@gmail.com>
2022-06-14 03:10:47 +0300

    create helpers for comment links

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -18,4 +18,20 @@ module CommentsHelper
       html
     end
   end
+
+  def edit_comment_link(article, comment)
+    return unless policy(comment).update?
+
+    link_to 'Edit', edit_article_comment_path(article, comment),
+            class: 'btn btn-primary'
+  end
+
+  def delete_comment_link(article, comment)
+    return unless policy(comment).destroy?
+
+    link_to 'Delete', article_comment_path(article, comment), data: {
+      method: :delete,
+      confirm: 'Are you sure?'
+    }, class: 'btn btn-danger'
+  end
 end
