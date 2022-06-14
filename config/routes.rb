bd11bd0bc618aed9557a13630fc2b99bb65b7d21
ixboy <ismaelixboy@gmail.com>
2022-06-14 03:11:55 +0300

    create edit views and routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
   resources :articles do
-    resources :comments, only: %i[create destroy]
+    resources :comments, only: %i[create destroy edit update]
   end
   resources :categories, except: :show
 end
