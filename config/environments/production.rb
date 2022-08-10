c8ab69fe9f68968ef1ba35d832c2351e2952f7d7
ixboy <ismaelixboy@gmail.com>
2022-08-10 04:06:59 +0100

    edit credentials and change master key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,13 +99,13 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'bootstrapblog.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'bootstrap-blog-rails.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: Rails.application.credentials.gmail[:username],
+    user_name: Rails.application.credentials.gmail[:user_name],
     password: Rails.application.credentials.gmail[:password],
     authentication: 'plain',
     enable_starttls_auto: true,
