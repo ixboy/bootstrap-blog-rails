b5ec3cec34cffb753344aae51951b5e3e8676cf0
ixboy <ismaelixboy@gmail.com>
2022-08-18 05:31:46 +0100

    add sender_mailer variable and setup production db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_06_04_160441) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.text "title"
     t.text "body"
