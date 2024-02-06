f4dea08668674539a1224adc7d7691b5b26642c1
Abigail Mugadza <abbymugadza@abigails-air.mynetworksettings.com>
2024-02-06 15:30:46 -0500

    correct typo from mailer host in devise innitializer file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'bootstrap-blog-rails.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'bootstrap-rails-blog-b7556c55d018.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
